d5325de0255aee2f44f7a474a22dd032957b259f
Sabanina Tetiana <tatiana08st@gmail.com>
2019-10-11 12:21:33 +0300

    Updates

---
+++ precache-manifest.0225801a287c37b307c84912a024567b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a85657b19be16b33b6bc2c307d6477b",
+    "url": "/tetianasabanina/redux-demo/index.html"
+  },
+  {
+    "revision": "a4d6d2f7027b84e3f634",
+    "url": "/tetianasabanina/redux-demo/static/css/main.5810b1bc.chunk.css"
+  },
+  {
+    "revision": "07369766b0a36b8c660f",
+    "url": "/tetianasabanina/redux-demo/static/js/2.357cc2d6.chunk.js"
+  },
+  {
+    "revision": "a4d6d2f7027b84e3f634",
+    "url": "/tetianasabanina/redux-demo/static/js/main.9a3e0666.chunk.js"
+  },
+  {
+    "revision": "96c23f82de99d3120b90",
+    "url": "/tetianasabanina/redux-demo/static/js/runtime-main.076b9019.js"
+  }
+]);
